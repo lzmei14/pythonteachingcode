26b3037d1f5bc0d395b5ae848288e42945fdfbd5
Michelle Liu <mliu10@students.kennesaw.edu>
2026-09-25 05:41:10 -0400

    P1M1
    Completed Module 1.1 & 1.2 on Jupyter Notebook. Created new file for 1.2 Required Code.

---
+++ P1 Python Absolute Beginner/P1M1MichelleLiu.py
@@ -0,0 +1,16 @@
+# Create name check code
+
+# [ ] get input for input_test variable
+input_test = input("Please input the names of people met in the last 24 hours: ").lower()
+
+# [ ] print "True" message if "John" is in the input or False message if not
+print("Met with John: ","john" in input_test)
+
+# [ ] print True message if your name is in the input or False if not
+print("Met with Michelle: ","michelle" in input_test)
+
+# [ ] Challenge: Check if another person's name is in the input - print message
+print("Met with Kira: ","kira" in input_test)
+
+# [ ] Challenge: Check if a fourth person's name is in the input - print message
+print("Met with Camila: ","camila" in input_test)
